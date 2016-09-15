a7256d1caa7c08f9ff158156c145d282284bd091
Arseniy Fomchenko <Arseniy Fomchenko>
2016-09-15 14:46:30 +0300

    win ready

--- hdd_info.py
+++ hdd_info.py
@@ -1,23 +1,62 @@
 import sys
 import os
 import subprocess
+from abc import ABCMeta, abstractmethod
 
-def list_hdds_win():
-    wmic = subprocess.Popen('wmic diskdrive get name,size', shell=True, stdout=subprocess.PIPE)
-    out, err = wmic.communicate()
-    drives = out.split('\n')[1:-2]
-    return '\n'.join(drives)
 
-def list_hdds():
+class OSHelper(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def list_hdds(self):
+        pass
+
+    @abstractmethod
+    def list_parts(self, disk_nr):
+        pass
+
+
+class WinHelper(OSHelper):
+    def run_diskpart(self, command):
+        fname = 'command.txt'
+        with open(fname, 'w') as f:
+            f.write(command)
+        dp = subprocess.Popen('diskpart /s {}'.format(fname), shell=True, stdout=subprocess.PIPE)
+        o,e = dp.communicate()
+        if dp.returncode == 0:
+            return o
+        else:
+            if o:
+                print o
+            else:
+                print("An error has occured. Please try running as administrator")
+
+    def list_hdds(self):
+        COMMAND = 'list disk'
+        output = self.run_diskpart(COMMAND)
+        if output:
+            return '\n'.join(output.split('\n')[6:])
+
+    def list_parts(self, n):
+        COMMAND = 'select disk {}\nlist partition'.format(int(n))
+        output = self.run_diskpart(COMMAND)
+        if output:
+            return '\n'.join(output.split('\n')[7:])
+
+
+def get_os_helper():
+    """
+    :rtype: OSHelper
+    """
     if os.name == 'nt':
-        return list_hdds_win()
+        return WinHelper()
     elif os.name == 'posix':
-        return list_hdds_unix()
-    else:
-        return 'Your system is not supported'
+        return UnixHelper()
+
 
 if __name__ == '__main__':
+    helper = get_os_helper()
     if len(sys.argv) == 1:
-        print(list_hdds())
+        print(helper.list_hdds())
     elif len(sys.argv) == 2:
-        print(list_partitions(sys.argv[1]))
+        print(helper.list_parts(sys.argv[1]))
