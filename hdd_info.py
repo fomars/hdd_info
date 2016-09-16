2b2b777b87e8b8cb969341d3a6608cabdd834142
arseny <fomars@gmail.com>
2016-09-16 14:25:24 +0300

    linux ok

--- hdd_info.py
+++ hdd_info.py
@@ -22,14 +22,14 @@ class WinHelper(OSHelper):
         with open(fname, 'w') as f:
             f.write(command)
         dp = subprocess.Popen('diskpart /s {}'.format(fname), shell=True, stdout=subprocess.PIPE)
-        o,e = dp.communicate()
+        output, errors = dp.communicate()
         if dp.returncode == 0:
-            return o
+            return output
         else:
-            if o:
-                print o
+            if output:
+                print output
             else:
-                print("An error has occured. Please try running as administrator")
+                print("An error has occurred. Please try running as administrator")
 
     def list_hdds(self):
         COMMAND = 'list disk'
@@ -42,6 +42,21 @@ class WinHelper(OSHelper):
         output = self.run_diskpart(COMMAND)
         if output:
             return '\n'.join(output.split('\n')[7:])
+
+
+class UnixHelper(OSHelper):
+    def list_parts(self, disk_name):
+        COMMAND = 'lsblk -l -o NAME,MAJ:MIN,SIZE,TYPE | grep {0:s} | awk -F" " \'{{print $1, $3}}\''.format(disk_name)
+        p = subprocess.Popen(COMMAND, shell=True, stdout=subprocess.PIPE)
+        o, e = p.communicate()
+        return o
+
+    def list_hdds(self):
+        COMMAND = 'lsblk -l -o NAME,MAJ:MIN,SIZE,TYPE | grep disk | awk -F" " \'{print $1, $3}\''
+        p = subprocess.Popen(COMMAND, shell=True, stdout=subprocess.PIPE)
+        o, e = p.communicate()
+        return o
+
 
 
 def get_os_helper():
---
+++ readme.md
@@ -0,0 +1,6 @@
+To list physical drives run:
+    python hdd_info.py
+
+To list drive partition run:
+    phython hdd_info.py $DISK
+where $DISK is drive name in Linux or drive number in Windows
